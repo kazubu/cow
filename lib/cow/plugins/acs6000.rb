1940a2df04668cff32695a0738c822e3c9144661
Kazuki Shimizu <kazubu@jtime.net>
2021-10-05 16:03:09 +0900

    ACS6000: Ignore non-CAS profile ports.

--- lib/cow/plugins/acs6000.rb
+++ lib/cow/plugins/acs6000.rb
@@ -4,6 +4,7 @@ require 'snmp'
 module Cow
   module ACS6000
     OID_DESCRIPTION_LIST = '1.3.6.1.4.1.10418.16.2.3.2.1.4'.freeze
+    OID_PORT_TABLE_PROFILE_LIST = '1.3.6.1.4.1.10418.16.2.3.2.1.5'.freeze
     TCP_PORT_OFFSET = 7000
 
     class Port < Cow::Tmpl::Port
@@ -18,8 +19,15 @@ module Cow
       def get_ports
         ret = []
         snmp do |s|
+          port_profile_tables = {}
+          s.walk(OID_PORT_TABLE_PROFILE_LIST) do |x|
+            port_profile_tables[x.name.last.to_s] = x.value.to_i
+          end
+
           s.walk(OID_DESCRIPTION_LIST) do |x|
             next if x.name.last.zero?
+            next if x.value.to_s.length == 0
+            next if port_profile_tables[x.name.last.to_s] != 1 # other than CAS Profile
             next unless (/^[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-p-[0-9]+$/ =~ x.value.to_s).nil?
             port = x.name.last
             name = x.value.to_s
